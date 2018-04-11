65652a63372e602393af0977f73444ec3a0a9cdb
bluebug <iopzx@gmail.com>
2018-04-11 01:55:06 -0400

    Created copy of core scripts
    I extended original classes.
    I don't know how much of editing original code is acceptable and how
    much is not. So I am not touching the original code at all.

---
+++ Assets/Scripts/Games/React2/ReactData2.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Linq;
+
+
+/// <summary>
+/// Contains general Game data for the React gametype.
+/// </summary>
+public class ReactData2 : ReactData
+{
+    public ReactData2(XmlElement elem)
+        : base(elem)
+    {
+    }
+}
---
+++ Assets/Scripts/Games/React2/ReactTrial2.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Linq;
+
+
+/// <summary>
+/// Contains Trial data for the React gameType.
+/// </summary>
+public class ReactTrial2 : ReactTrial
+{
+    public ReactTrial2(SessionData data, XmlElement n = null)
+        : base(data, n)
+    {
+    }
+}
